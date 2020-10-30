fc854afb1300cda0274aed0ab619f39c10c3d71a
Kendrick Filbert <72587399+kendrickassignemt@users.noreply.github.com>
2020-10-30 16:09:16 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,409 @@
+import React from 'react';
+// import logo from './logo.svg';
+import Portfolio from './components/Portfolio'
+import './App.css';
+
+function App() {
+  const portfolioLinks = [
+    {
+      title: 'Threads',
+      caption: 'Illustration'
+    },
+    {
+      title: 'Explore',
+      caption: 'Graphic Design'
+    },
+    {
+      title: 'Finish',
+      caption: 'Identity'
+    },
+    {
+      title: 'Lines',
+      caption: 'Branding'
+    },
+    {
+      title: 'Southwest',
+      caption: 'Website Design'
+    },
+    {
+      title: 'Window',
+      caption: 'Photography'
+    },
+    {
+      title: 'Pizza',
+      caption: 'I love pizza!'
+    }
+  ]
+
+  return (
+    <div className="App">
+      <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+    <div className="container">
+      <a className="navbar-brand js-scroll-trigger" href="#page-top">Made by React.js</a>
+      <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        Menu
+        <i className="fa fa-bars"></i>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarResponsive">
+        <ul className="navbar-nav text-uppercase ml-auto">
+          <li className="nav-item">
+            <a className="nav-link js-scroll-trigger" href="#services">Services</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link js-scroll-trigger" href="#about">About</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link js-scroll-trigger" href="#team">Team</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link js-scroll-trigger" href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  
+  <header className="masthead">
+    <div className="container">
+      <div className="intro-text">
+        <div className="intro-lead-in">Welcome To Ken Page !</div>
+        <div className="intro-heading text-uppercase">Welcome To Ken's</div>
+        <a className="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">About Me</a>
+      </div>
+    </div>
+  </header>
+
+  
+  <section className="page-section" id="services">
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12 text-center">
+          <h2 className="section-heading text-uppercase">Services</h2>
+          <h3 className="section-subheading text-muted">My React.js Website consist by.</h3>
+        </div>
+      </div>
+      <div className="row text-center">
+        <div className="col-md-4">
+          <span className="fa-stack fa-4x">
+            <i className="fa fa-circle fa-stack-2x text-primary"></i>
+            <i className="fa fa-shopping-cart fa-stack-1x fa-inverse"></i>
+          </span>
+          <h4 className="service-heading">E-Commerce</h4>
+          <p className="text-muted">Build Responsive Old Amazona Website by using node.js, React.js and MongoDB</p>
+        </div>
+        <div className="col-md-4">
+          <span className="fa-stack fa-4x">
+            <i className="fa fa-circle fa-stack-2x text-primary"></i>
+            <i className="fa fa-laptop fa-stack-1x fa-inverse"></i>
+          </span>
+          <h4 className="service-heading">Responsive Design</h4>
+          <p className="text-muted">Build several website including this one. Check github.com/kendrickassignemt / kendrickportfolio.000webhostapp.com</p>
+        </div>
+        <div className="col-md-4">
+          <span className="fa-stack fa-4x">
+            <i className="fa fa-circle fa-stack-2x text-primary"></i>
+            <i className="fa fa-lock fa-stack-1x fa-inverse"></i>
+          </span>
+          <h4 className="service-heading">Another to come</h4>
+          <p className="text-muted">I will make another website. Coming soon.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  
+  <Portfolio portfolioLinks={portfolioLinks}></Portfolio>
+
+  
+  <section className="page-section" id="about">
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12 text-center">
+          <h2 className="section-heading text-uppercase">About</h2>
+          <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-lg-12">
+          <ul className="timeline">
+            <li>
+              <div className="timeline-image">
+                <img className="rounded-circle img-fluid" src="img/about/1.jpg" alt=""/>
+              </div>
+              <div className="timeline-panel">
+                <div className="timeline-heading">
+                  <h4>2016</h4>
+                  <h4 className="subheading">My Web Journey</h4>
+                </div>
+                <div className="timeline-body">
+                  <p className="text-muted">When i studied web design and development. Started to loved this</p>
+                </div>
+              </div>
+            </li>
+            <li className="timeline-inverted">
+              <div className="timeline-image">
+                <img className="rounded-circle img-fluid" src="img/about/2.jpg" alt=""/>
+              </div>
+              <div className="timeline-panel">
+                <div className="timeline-heading">
+                  <h4>2017</h4>
+                  <h4 className="subheading">Passion is born among me</h4>
+                </div>
+                <div className="timeline-body">
+                  <p className="text-muted">Started to improving by making some responsive website (front end) and back end that consist login and register</p>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="timeline-image">
+                <img className="rounded-circle img-fluid" src="img/about/3.jpg" alt=""/>
+              </div>
+              <div className="timeline-panel">
+                <div className="timeline-heading">
+                  <h4>2018</h4>
+                  <h4 className="subheading">My first project</h4>
+                </div>
+                <div className="timeline-body">
+                  <p className="text-muted">Glad to do my first real project : Musicatalog</p>
+                </div>
+              </div>
+            </li>
+            <li className="timeline-inverted">
+              <div className="timeline-image">
+                <img className="rounded-circle img-fluid" src="img/about/4.jpg" alt=""/>
+              </div>
+              <div className="timeline-panel">
+                <div className="timeline-heading">
+                  <h4>2019</h4>
+                  <h4 className="subheading">Time to learn new web framework </h4>
+                </div>
+                <div className="timeline-body">
+                  <p className="text-muted">Learning new framwork react.js, laravel</p>
+                </div>
+              </div>
+            </li>
+            <li className="timeline-inverted">
+              <div className="timeline-image">
+                <h4>Be Part
+                  <br/>Of Our
+                  <br/>Story!</h4>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  
+  <section className="bg-light page-section" id="team">
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12 text-center">
+          <h2 className="section-heading text-uppercase">Our Amazing Team</h2>
+          <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm-4">
+          <div className="team-member">
+            <img className="mx-auto rounded-circle" src="assets/img/team/1.jpg" alt=""/>
+            <h4>Kendrick F</h4>
+            <p className="text-muted">Lead Designer</p>
+            <ul className="list-inline social-buttons">
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="fa fa-twitter"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="fa fa-facebook-f"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="fa fa-linkedin-in"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="col-sm-4">
+          <div className="team-member">
+            <img className="mx-auto rounded-circle" src="img/team/2.jpg" alt=""/>
+            <h4>Kendrick</h4>
+            <p className="text-muted">Lead Marketer</p>
+            <ul className="list-inline social-buttons">
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="fa fa-twitter"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="fa fa-facebook-f"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="fa fa-linkedin-in"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="col-sm-4">
+          <div className="team-member">
+            <img className="mx-auto rounded-circle" src="img/team/3.jpg" alt=""/>
+            <h4>More</h4>
+            <p className="text-muted">Lead Developer</p>
+            <ul className="list-inline social-buttons">
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="fa fa-twitter"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="fa fa-facebook-f"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="linkedin.com">
+                  <i className="fa fa-linkedin-in"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-lg-8 mx-auto text-center">
+          <p className="large text-muted">Very glad to working as tema</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  
+  <section className="py-5">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-3 col-sm-6">
+          <a href="#something">
+            <img className="img-fluid d-block mx-auto" src="img/logos/envato.jpg" alt=""/>
+          </a>
+        </div>
+        <div className="col-md-3 col-sm-6">
+          <a href="#something">
+            <img className="img-fluid d-block mx-auto" src="img/logos/designmodo.jpg" alt=""/>
+          </a>
+        </div>
+        <div className="col-md-3 col-sm-6">
+          <a href="#something">
+            <img className="img-fluid d-block mx-auto" src="img/logos/themeforest.jpg" alt=""/>
+          </a>
+        </div>
+        <div className="col-md-3 col-sm-6">
+          <a href="#something">
+            <img className="img-fluid d-block mx-auto" src="img/logos/creative-market.jpg" alt=""/>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  
+  <section className="page-section" id="contact">
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12 text-center">
+          <h2 className="section-heading text-uppercase">Contact Us</h2>
+          <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-lg-12">
+          <form id="contactForm" name="sentMessage" novalidate="novalidate">
+            <div className="row">
+              <div className="col-md-6">
+                <div className="form-group">
+                  <input className="form-control" id="name" type="text" placeholder="Your Name *" required="required" data-validation-required-message="Please enter your name."/>
+                  <p className="help-block text-danger"></p>
+                </div>
+                <div className="form-group">  
+                  <input className="form-control" id="email" type="email" placeholder="Your Email *" required="required" data-validation-required-message="Please enter your email address."/>
+                  <p className="help-block text-danger"></p>
+                </div>
+                <div className="form-group">
+                  <input className="form-control" id="phone" type="tel" placeholder="Your Phone *" required="required" data-validation-required-message="Please enter your phone number."/>
+                  <p className="help-block text-danger"></p>
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div className="form-group">
+                  <textarea className="form-control" id="message" placeholder="Your Message *" required="required" data-validation-required-message="Please enter a message."></textarea>
+                  <p className="help-block text-danger"></p>
+                </div>
+              </div>
+              <div className="clearfix"></div>
+              <div className="col-lg-12 text-center">
+                <div id="success"></div>
+                <button id="sendMessageButton" className="btn btn-primary btn-xl text-uppercase" type="submit">Send Message</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  
+  <footer className="footer">
+    <div className="container">
+      <div className="row align-items-center">
+        <div className="col-md-4">
+          <span className="copyright">Copyright &copy; Your Website 2019</span>
+        </div>
+        <div className="col-md-4">
+          <ul className="list-inline social-buttons">
+            <li className="list-inline-item">
+              <a href="#something">
+                <i className="fa fa-twitter"></i>
+              </a>
+            </li>
+            <li className="list-inline-item">
+              <a href="#something">
+                <i className="fa fa-facebook-f"></i>
+              </a>
+            </li>
+            <li className="list-inline-item">
+              <a href="#something">
+                <i className="fa fa-linkedin-in"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-md-4">
+          <ul className="list-inline quicklinks">
+            <li className="list-inline-item">
+              <a href="#something">Privacy Policy</a>
+            </li>
+            <li className="list-inline-item">
+              <a href="#something">Terms of Use</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+    </div>
+  );
+}
+
+export default App;
